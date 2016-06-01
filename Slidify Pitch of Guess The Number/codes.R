614410397970bf9e2541e0a5e0e5766a03fafbae
prabinchitrakar <pchitrakar@veriskhealth.com>
2016-06-01 07:02:31 -0600

    Update codes.R

--- Slidify Pitch of Guess The Number/codes.R
+++ Slidify Pitch of Guess The Number/codes.R
@@ -3,8 +3,8 @@ install.packages("devtools")
 library(devtools)
 
 #install and load Slidify
-install_github('slidify', 'rajus74')
-install_github('slidifyLibraries', 'rajus74')
+install_github('slidify', 'prabinchitrakar')
+install_github('slidifyLibraries', 'prabinchitrakar')
 library(slidify)
 
 author("Guess_The_Number")
